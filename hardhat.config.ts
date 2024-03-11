f2384b28d97053364cfedac7a8798e34553df315
David Potolski Lafetá <davidpotolskilafeta@gmail.com>
2024-03-11 14:50:10 -0300

    Adds Rollux on deployment config

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -4,6 +4,8 @@ import {HardhatUserConfig} from 'hardhat/config';
 // @ts-ignore
 import {accounts} from './test-wallets.js';
 import {eEthereumNetwork} from './helpers/types';
+import dotenv from 'dotenv';
+
 import {BUIDLEREVM_CHAINID, COVERAGE_CHAINID} from './helpers/buidler-constants';
 
 import '@nomiclabs/hardhat-ethers';
@@ -13,14 +15,12 @@ import 'hardhat-gas-reporter';
 import 'hardhat-typechain';
 import 'solidity-coverage';
 
+dotenv.config();
+
 const SKIP_LOAD = process.env.SKIP_LOAD === 'true';
 const DEFAULT_BLOCK_GAS_LIMIT = 12450000;
-const DEFAULT_GAS_PRICE = 10;
-const HARDFORK = 'istanbul';
-const INFURA_KEY = process.env.INFURA_KEY || '';
+const PRIVATE_KEY = process.env.PRIVATE_KEY || '';
 const ETHERSCAN_KEY = process.env.ETHERSCAN_KEY || '';
-const MNEMONIC_PATH = "m/44'/60'/0'/0";
-const MNEMONIC = process.env.MNEMONIC || '';
 
 // Prevent to load scripts before compilation and typechain
 if (!SKIP_LOAD) {
@@ -35,22 +35,6 @@ if (!SKIP_LOAD) {
 }
 
 require(`${path.join(__dirname, 'tasks/misc')}/set-DRE.ts`);
-
-const getCommonNetworkConfig = (networkName: eEthereumNetwork, networkId: number) => {
-  return {
-    url: `https://${networkName}.infura.io/v3/${INFURA_KEY}`,
-    hardfork: HARDFORK,
-    blockGasLimit: DEFAULT_BLOCK_GAS_LIMIT,
-    gasMultiplier: DEFAULT_GAS_PRICE,
-    chainId: networkId,
-    accounts: {
-      mnemonic: MNEMONIC,
-      path: MNEMONIC_PATH,
-      initialIndex: 0,
-      count: 20,
-    },
-  };
-};
 
 const buidlerConfig: HardhatUserConfig = {
   solidity: {
@@ -88,10 +72,13 @@ const buidlerConfig: HardhatUserConfig = {
   mocha: {
     timeout: 0,
   },
+  defaultNetwork: 'hardhat',
   networks: {
-    kovan: getCommonNetworkConfig(eEthereumNetwork.kovan, 42),
-    ropsten: getCommonNetworkConfig(eEthereumNetwork.ropsten, 3),
-    main: getCommonNetworkConfig(eEthereumNetwork.main, 1),
+    main: {
+      chainId: 570,
+      url: 'https://rpc.rollux.com',
+      accounts: [PRIVATE_KEY],
+    },
     hardhat: {
       hardfork: 'istanbul',
       blockGasLimit: DEFAULT_BLOCK_GAS_LIMIT,
@@ -104,25 +91,6 @@ const buidlerConfig: HardhatUserConfig = {
         privateKey: secretKey,
         balance,
       })),
-    },
-    buidlerevm_docker: {
-      hardfork: 'istanbul',
-      blockGasLimit: 9500000,
-      gas: 9500000,
-      gasPrice: 8000000000,
-      chainId: BUIDLEREVM_CHAINID,
-      throwOnTransactionFailures: true,
-      throwOnCallFailures: true,
-      url: 'http://localhost:8545',
-    },
-    ganache: {
-      url: 'http://ganache:8545',
-      accounts: {
-        mnemonic: 'fox sight canyon orphan hotel grow hedgehog build bless august weather swarm',
-        path: "m/44'/60'/0'/0",
-        initialIndex: 0,
-        count: 20,
-      },
     },
   },
 };
